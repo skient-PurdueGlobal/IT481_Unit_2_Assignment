89a9e7729915cef6dbf9c8f0f088ddba0a3f670a
skienterprises-da <douglas_andrzejewski@ski-network.net>
2021-12-16 13:45:49 -0500

    Add project files.

---
+++ BAL/BusinessLayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT481_Unit_2_Assignment.BAL
+{
+    class BusinessLayer
+    {
+    }
+}
---
+++ DAL/DBAccessLayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT481_Unit_2_Assignment.DAL
+{
+    class DBAccessLayer
+    {
+    }
+}
